cd22ba5a87645b46aa00b007bda5f9835cf5b215
AKunshin <reyiby2015@gmail.com>
2023-01-18 16:03:18 +0300

    Использование amount в url

--- avto_numbers/plate/numbers_gen.py
+++ avto_numbers/plate/numbers_gen.py
@@ -2,25 +2,20 @@ import random
 from loguru import logger
 
 
-def gos_numbers_gen(amount: int) -> str:
+def gos_numbers_gen() -> str:
     # Генерация гос. номера
-    i = 0
-    gos_numbers = []
     letters = ['А', 'В', 'Е', 'К', 'М', 'Н', 'О', 'Р', 'С', 'Т', 'У', 'Х']
-    while i < amount:
-        numbers = random.randint(1, 999)
-        if numbers < 10:
-            numbers = '00' + str(numbers)
-        elif numbers < 100:
-            numbers = '0' + str(numbers)
+    numbers = random.randint(1, 999)
+    if numbers < 10:
+        numbers = '00' + str(numbers)
+    elif numbers < 100:
+        numbers = '0' + str(numbers)
 
-        gos_number = random.choice(letters) + str(numbers) + \
-            random.choice(letters) + random.choice(letters)
-        gos_numbers.append(gos_number)
-        i += 1
-    logger.debug(gos_numbers)
-    return gos_numbers
+    gos_number = random.choice(letters) + str(numbers) + \
+        random.choice(letters) + random.choice(letters)
+    return gos_number
 
 
 if __name__ == "__main__":
-    gos_numbers_gen(3)
+    gos_numbers = gos_numbers_gen(3)
+    logger.debug(gos_numbers)
--- avto_numbers/plate/views.py
+++ avto_numbers/plate/views.py
@@ -13,20 +13,21 @@ class GosNumberViewSet(ModelViewSet):
 
     @action(methods=['get'], detail=False)
     def generate(self, request):
-        logger.debug(request)
-        gen_number = gos_numbers_gen(3)
+        amount = request.GET.get('amount')
+        if not amount:
+            number = gos_numbers_gen()
+            logger.debug(f"Не передан amount, вывод одного номера: {number}")
+            GosNumber.objects.create(number=number)
+            gosnumber = GosNumber.objects.all().last()
+            serializer = GosNumberSerializer(gosnumber)
+            return Response(serializer.data)
         i = 0
-        while i < 3:
-            number = gen_number[i]
-            logger.debug(f"Gos Number {number}")
+        amount = int(amount)
+        while i < amount:
+            number = gos_numbers_gen()
+            logger.debug(f"Gos Number: {number}")
             GosNumber.objects.create(number=number)
             i += 1
-        gosnumbers = GosNumber.objects.all().order_by('-pk')[:3]
+        gosnumbers = GosNumber.objects.all().order_by('-pk')[:amount]
         serializer = GosNumberSerializer(gosnumbers, many=True)
         return Response(serializer.data)
-
-
-
-
-        
-

e77266f63e63b8cfe54ee0c4b29caaaf14517567
AKunshin <reyiby2015@gmail.com>
2023-01-25 09:58:52 +0300

    Реализован Plate-Get по uuid

--- avto_numbers/plate/views.py
+++ avto_numbers/plate/views.py
@@ -1,9 +1,10 @@
+from loguru import logger
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny
 from rest_framework import status
-from loguru import logger
+from django.core.exceptions import ValidationError
 from .serializers import GosNumberSerializer
 from .models import GosNumber
 from .numbers_gen import generate_gos_numbers
@@ -12,7 +13,6 @@ from .numbers_gen import generate_gos_numbers
 class GosNumberViewSet(ModelViewSet):
     queryset = GosNumber.objects.all()
     serializer_class = GosNumberSerializer
-    # permission_classes = [IsAuthenticated]
     permission_classes = [AllowAny]
 
     @action(methods=['get'], detail=False)
@@ -25,7 +25,7 @@ class GosNumberViewSet(ModelViewSet):
             GosNumber.objects.create(number=number)
             last_gosnumber = GosNumber.objects.all().last()
             serializer = GosNumberSerializer(last_gosnumber)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         i = 0
         amount = int(amount)
         while i < amount:
@@ -35,13 +35,17 @@ class GosNumberViewSet(ModelViewSet):
             i += 1
         gosnumbers = GosNumber.objects.all().order_by('-pk')[:amount]
         serializer = GosNumberSerializer(gosnumbers, many=True)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @action(methods=['get'], detail=False)
     def get(self, request):
         # Метод для вывода одной записи по uuid
         uuid = request.GET.get('id')
-        logger.debug(f"UUID: {uuid}")
-        gosnumber_detail = GosNumber.objects.get(uuid=uuid)
-        serializer = GosNumberSerializer(gosnumber_detail)
+        if not uuid:
+            return Response({'detail': 'Не передан uuid'})
+        try:
+            gosnumber_detail = GosNumber.objects.get(uuid=uuid)
+            serializer = GosNumberSerializer(gosnumber_detail)
+        except ValidationError:
+            return Response({'detail': 'Неверный uuid'})
         return Response(serializer.data, status=status.HTTP_200_OK)

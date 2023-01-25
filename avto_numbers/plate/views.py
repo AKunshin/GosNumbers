d7fdf20fa6e59d76e7f5a21f32ee05df9cf9fdc3
AKunshin <reyiby2015@gmail.com>
2023-01-25 15:07:44 +0300

    Реализована проверка передаваемого клиентом номера

--- avto_numbers/plate/views.py
+++ avto_numbers/plate/views.py
@@ -1,8 +1,9 @@
+import re
 from loguru import logger
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from django.core.exceptions import ValidationError
 from .serializers import GosNumberSerializer
@@ -13,7 +14,7 @@ from .numbers_gen import generate_gos_numbers
 class GosNumberViewSet(ModelViewSet):
     queryset = GosNumber.objects.all()
     serializer_class = GosNumberSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
     @action(methods=['get'], detail=False)
     def generate(self, request):
@@ -22,8 +23,7 @@ class GosNumberViewSet(ModelViewSet):
         if not amount:
             number = generate_gos_numbers()
             logger.debug(f"Не передан amount, вывод одного номера: {number}")
-            GosNumber.objects.create(number=number)
-            last_gosnumber = GosNumber.objects.all().last()
+            last_gosnumber = GosNumber.objects.create(number=number)
             serializer = GosNumberSerializer(last_gosnumber)
             return Response(serializer.data, status=status.HTTP_200_OK)
         i = 0
@@ -42,10 +42,25 @@ class GosNumberViewSet(ModelViewSet):
         # Метод для вывода одной записи по uuid
         uuid = request.GET.get('id')
         if not uuid:
-            return Response({'detail': 'Не передан uuid'})
+            return Response({'detail': 'Не передан uuid'}, status=status.HTTP_400_BAD_REQUEST)
         try:
             gosnumber_detail = GosNumber.objects.get(uuid=uuid)
             serializer = GosNumberSerializer(gosnumber_detail)
         except ValidationError:
-            return Response({'detail': 'Неверный uuid'})
+            return Response({'detail': 'Неверный uuid'}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    @action(methods=['post'], detail=False)
+    def add(self, request):
+        # Метод для добавления переданного клиентом гос.номера в БД
+        plate = request.GET.get('plate').upper()
+        logger.debug(f"Полученный номер: {plate}")
+        if not plate:
+            return Response({'detail':'Не передан гос. номер'}, status=status.HTTP_400_BAD_REQUEST)
+        tpl = r'[АВЕКМНОРСТУХ]{1}\d{3}[АВЕКМНОРСТУХ]{2}$'
+        if not re.match(tpl, plate):
+            return Response({'detail': 'Ошибка в гос. номере'}, status=status.HTTP_400_BAD_REQUEST)
+        new_number = GosNumber.objects.create(number=plate)
+        serializer = GosNumberSerializer(new_number)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
